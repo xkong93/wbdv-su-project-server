2beb848095c3d7cb26f1c58ea71b5f024b588695
XuanKong <k.xuan29@gmail.com>
2019-08-02 09:19:33 -0700

    add exception in review api

--- src/main/java/com/example/wbdvsu19projectserver/controller/ReviewController.java
+++ src/main/java/com/example/wbdvsu19projectserver/controller/ReviewController.java
@@ -4,6 +4,7 @@ import com.example.wbdvsu19projectserver.models.Review;
 import com.example.wbdvsu19projectserver.sevices.ReviewService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +13,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author Xuan Kong
@@ -58,9 +62,8 @@ public class ReviewController {
   public List<Review> getAllReviewsForProductByUrlkey(@PathVariable("urlKey") String urlKey) {
     try {
       return reviewService.getAllReviewsForProductByUrlKey(urlKey);
-
     }catch (Exception e){
-      return null;
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND,"REVIEW NOT FOUND", e);
     }
   }
   @PutMapping("api/review/{rid}")
