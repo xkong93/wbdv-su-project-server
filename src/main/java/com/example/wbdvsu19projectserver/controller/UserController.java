55fe260716eb8b3a44f012896cfe417225dc92ae
XuanKong <k.xuan29@gmail.com>
2019-08-17 22:27:50 -0700

    update

--- src/main/java/com/example/wbdvsu19projectserver/controller/UserController.java
+++ src/main/java/com/example/wbdvsu19projectserver/controller/UserController.java
@@ -58,7 +58,7 @@ public class UserController {
       Cookie cookie = new Cookie("JSESSIONID", session.getId());
       cookie.setMaxAge(30 * 60);// set expire time to 30 mins
       cookie.setPath("/");
-//      cookie.setSecure(true);
+      cookie.setSecure(true);
 //      cookie.setHttpOnly(true);
 //      cookie.setDomain("calm-taiga-99221.herokuapp.com");
       response.addCookie(cookie);
