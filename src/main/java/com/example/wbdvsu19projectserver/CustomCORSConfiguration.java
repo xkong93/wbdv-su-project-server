c8bd84faa03addad1977e32f4ae8a3c7e5107669
XuanKong <k.xuan29@gmail.com>
2019-08-18 00:03:07 -0700

    update

--- src/main/java/com/example/wbdvsu19projectserver/CustomCORSConfiguration.java
+++ src/main/java/com/example/wbdvsu19projectserver/CustomCORSConfiguration.java
@@ -16,7 +16,7 @@ public class CustomCORSConfiguration {
   private CorsConfiguration buildConfig() {
     CorsConfiguration corsConfiguration = new CorsConfiguration();
     corsConfiguration.setAllowCredentials(true); //kep point!! required
-    corsConfiguration.addAllowedOrigin("http://calm-taiga-99221.herokuapp.com");
+    corsConfiguration.addAllowedOrigin("http://www.sneakerjunkies.life");
     corsConfiguration.addAllowedHeader("*");
     corsConfiguration.addAllowedMethod("*");
     corsConfiguration.addExposedHeader("Set-Cookie");
