effd1fbc4c91a0b1923c0018e436c6a01e70a1e4
XuanKong <k.xuan29@gmail.com>
2019-08-17 22:08:25 -0700

    update

--- src/main/java/com/example/wbdvsu19projectserver/controller/UserController.java
+++ src/main/java/com/example/wbdvsu19projectserver/controller/UserController.java
@@ -57,8 +57,8 @@ public class UserController {
       Cookie cookie = new Cookie("JSESSIONID", session.getId());
       cookie.setMaxAge(30 * 60);// set expire time to 30 mins
       cookie.setPath("/");
-      cookie.setSecure(true);
-      cookie.setHttpOnly(true);
+      cookie.setSecure(false);
+//      cookie.setHttpOnly(true);
 //      cookie.setDomain("*.herokuapp.com");
       response.addCookie(cookie);
       ObjectMapper mapper = new ObjectMapper();
